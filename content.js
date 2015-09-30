658ee4ffc587fd5c33150fe582f20c0c9eb5f41f
lacunahag <jasmine.yhumbert@gmail.com>
2015-09-30 15:34:15 -0700

    First commit to capturing spans

--- content.js
+++ content.js
@@ -1,30 +1,38 @@
+function changeText(text) {
+  switch (true) { //detect the length and get a fun fact from the appropriate array
+    case (text.length < 40):
+      replacedText = shortest[Math.floor((Math.random() * shortest[0]) + 1)];
+      break;
+    case (text.length < 80):
+      replacedText = shortSentences[Math.floor((Math.random() * shortSentences[0]) + 1)];
+      break;
+    case (text.length < 120):
+      replacedText = longSentences[Math.floor((Math.random() * longSentences[0]) + 1)];
+      break;
+    default:
+      replacedText = shortParagraphs[Math.floor((Math.random() * shortParagraphs[0]) + 1)];
+  }
+  element.replaceChild(document.createTextNode(replacedText), node); // replace the text with the replaced text
+  $(element).addClass('ITORM-changed'); //and add a special css style
+}
 
-var elements = document.getElementsByTagName('*');
+var elements = document.getElementsByTagName('*'); //get all the elements from the document
 
-for (var i = 0; i < elements.length; i++) {
+var noSpanCount = 0; //for debugging
+var spanCount = 0;
+
+for (var i = 0; i < elements.length; i++) { //for each element
   var element = elements[i];
 
-  for (var j = 0; j < element.childNodes.length; j++) {
+  for (var j = 0; j < element.childNodes.length; j++) { //for each
     var node = element.childNodes[j];
-
-    if (node.nodeType === 3) {
-      var text = node.nodeValue;
-      if (text.indexOf('Trump') >= 0) {
-        switch(true) {
-            case (text.length < 40):
-                replacedText = shortest[Math.floor((Math.random() * shortest[0]) +1)];
-                break;
-            case (text.length < 80):
-                replacedText = shortSentences[Math.floor((Math.random() * shortSentences[0]) +1)];
-                break;
-            case (text.length < 120):
-                replacedText = longSentences[Math.floor((Math.random() * longSentences[0]) +1)];
-                break;
-            default:
-                replacedText = shortParagraphs[Math.floor((Math.random() * shortParagraphs[0]) +1)];
-        }
-        element.replaceChild(document.createTextNode(" " + replacedText + " "), node);
-        $(element).addClass('ITORM-changed');
+    if (node.nodeValue != null) {
+      if (node.nodeValue.indexOf('Trump') >= 0 && node.nodeType === 3 && node.nodeName !== 'SPAN' && node.parentNode.nodeName !== 'SPAN') { //if it's a non-span text node with Trump in it
+        changeText(node.nodeValue);
+      } else if (node.nodeValue.indexOf('Trump') >= 0 && node.nodeName == 'SPAN' && node.parentNode.nodeName != null) { // else if it's a span node with a parent
+        changeText(node.parentNode.nodeValue); // change the text of it's parent node
+        console.log("span #" + spanCount);
+        spanCount++;
       }
     }
   }
--- funfacts.js
+++ funfacts.js
@@ -1,26 +1,25 @@
 var shortest = [ //less than 40 chars
-  3,
+  56,
   "Mexicans invented chocolate",
   "The capital of Mexico is Mexico City",
   "The capital of Afghanistan is Kabul",
-  //"The capital of Algeria is Algiers",
-  //"The capital of Australia is Canberra",
-  //"The capital of Bangladesh is Dhaka",
-  //"The capital of China is Beijing",
-  // "The capital of France is Paris",
-  // "The capital of Italy is Rome",
-  // "The capital of Bolivia is La Paz",
-  // "The capital of Brazil is Brasilia",
-  // "The capital of Canada is Ottawa",
-  // "The capital of Chile is Santiago",
-  // "The capital of Colombia is Bogota",
-  // "The capital of Cuba is Havana",
-  // "The capital of Djibouti is Djibouti",
-  /*"The capital of Equador is Quito",
+  "The capital of Algeria is Algiers",
+  "The capital of Australia is Canberra",
+  "The capital of Bangladesh is Dhaka",
+  "The capital of China is Beijing",
+  "The capital of France is Paris",
+  "The capital of Italy is Rome",
+  "The capital of Bolivia is La Paz",
+  "The capital of Brazil is Brasilia",
+  "The capital of Canada is Ottawa",
+  "The capital of Chile is Santiago",
+  "The capital of Colombia is Bogota",
+  "The capital of Cuba is Havana",
+  "The capital of Djibouti is Djibouti",
+  "The capital of Equador is Quito",
   "The capital of Eritrea is Asmara",
   "The capital of Ghana is Accra",
   "The capital of Greece is Athens",
-  "The capital of Haiti is Port au Prince",
   "The capital of Ireland is Dublin",
   "The capital of Japan is Tokyo",
   "The capital of Jamaica is Kingston",
@@ -29,16 +28,12 @@ var shortest = [ //less than 40 chars
   "The capital of Liberia is Monrovia",
   "The capital of Lybia is Tripoli",
   "The capital of Macedonia is Skopje",
-  "The capital of Malaysia is Kuala Lumpur",
-  "The capital of Mongolia is Ulaanbaatar",
   "The capital of Iran is Tehran",
   "The capital of Iraq is Baghdad",
   "The capital of Germany is Berlin",
   "The capital of Malawi is Lilongwe",
   "The capital of Nepal is Kathmandu",
-  "The capital of New Zealand is Wellington",
   "The capital of Nigeria is Abuja",
-  "The capital of North Korea is Pyongyang",
   "The capital of Oman is Muscat",
   "The capital of Pakistan is Islamabad",
   "The capital of Qatar is Doha",
@@ -59,41 +54,44 @@ var shortest = [ //less than 40 chars
   "The capital of Tunisia is Tunis",
   "The capital of Turkey is Ankara",
   "The capital of Vietnam is Hanoi",
-  "The capital of Zimbabwe is Harare",*/
-  //"Mexican independence day is on 9/16"
+  "The capital of Zimbabwe is Harare",
+  "Mexican independence day is on 9/16"
 ];
 
 var shortSentences = [ // between 40 and 80 chars
-  2,
+  11,
   "More than half of the coastline of the entire United States is in Alaska.",
   "The first condoms appeared in 3,000 B.C.",
-  // "The birth control pill was introduced by a devout Catholic doctor.",
-  // "Mexico introduced chocolate, corn, and chilies to the world.",
-  // "The first printing press in North America was used in Mexico City in 1539.",
-  // "Mexico is second only to Brazil in the number of Catholic citizens."
+  "The birth control pill was introduced by a devout Catholic doctor.",
+  "Mexico introduced chocolate, corn, and chilies to the world.",
+  "The first printing press in North America was used in Mexico City in 1539.",
+  "The capital of New Zealand is Wellington",
+  "The capital of Malaysia is Kuala Lumpur",
+  "The capital of Mongolia is Ulaanbaatar",
+  "The capital of North Korea is Pyongyang",
+  "The capital of Haiti is Port au Prince",
+  "Mexico is second only to Brazil in the number of Catholic citizens."
 ];
 
-/*
 
 var longSentences = [ //between 80 and 120 chars
-  0,
-  "",
-  ""
+  2,
+  "The world lost about 16% of all coral reefs in 1998, the second hottest year on record. Mexico is second only to Brazil in the number of Catholic citizens.",
+  "The US releases more carbon dioxide than any other country, though it is home to just 5% of the world's population. "
 ];
-*/
 
 
 var shortParagraphs = [ //between 120 and 200 chars
-  2,
+  9,
   "Jose Hernandez-Rebollar invented the Acceleglove, a glove that can translate sign language into speech. According the Smithsonian, \"by using sensors attached to the glove and the arm, this prototype device can currently translate the alphabet and over 300 words in American Sign Language (ASL) into both English and Spanish.\"",
   "Coral reefs are as important to marine wildlife as rainforests are to land-based wildlife. Rising seawater temperatures threaten their survival. The world lost about 16% of all coral reefs in 1998, the second hottest year on record.",
-  // "Rain forest destruction contributes to climate change. That's because trees store carbon dioxide as they grow. Clearing and burning forests releases large amounts of carbon dioxide into the atmosphere.",
-  // "The United States releases more carbon dioxide than any other country, though it is home to just five percent of the world's population. If everyone in the world lived the way people do in the U.S., it would take five Earths to provide enough resources for everyone!",
-  // "The Intergovernmental Panel on Climate Change projects that temperatures will rise 3.2 - 7.2 degrees Fahrenheit in the 21st century. Dramatically reducing levels of global warming pollution may keep the rise in the lower part of this range.",
-  // "In general, the greenhouse effect is a good thing. Without greenhouse gases, the temperature on Earth would be too cold to support life. With too many, it would heat up beyond survivable levels. Earth is sometimes called the Goldilocks Planet because it is \"just right.\"",
-  // "Mexican children do not receive presents on Christmas Day. They receive gifts on January 6, the day on which Mexicans celebrate the arrival of the Three Wise Men.",
-  // "Mexico City is built over the ruins of a great Aztec city, Tenochtitlán. Because it is built on a lake, Mexico is sinking at a rate of 6 to 8 inches a year as pumps draw water out for the city’s growing population.",
-  // "Mexico’s flag is made up of three vertical stripes. The left green stripe stands for hope, the middle white stripe represents purity, and the right red stripe represents the blood of the Mexican people. The picture of an eagle eating a snake is based on an Aztec legend."
+  "Rain forest destruction contributes to climate change. That's because trees store carbon dioxide as they grow. Clearing and burning forests releases large amounts of carbon dioxide into the atmosphere.",
+  "The United States releases more carbon dioxide than any other country, though it is home to just five percent of the world's population. If everyone in the world lived the way people do in the U.S., it would take five Earths to provide enough resources for everyone!",
+  "The Intergovernmental Panel on Climate Change projects that temperatures will rise 3.2 - 7.2 degrees Fahrenheit in the 21st century. Dramatically reducing levels of global warming pollution may keep the rise in the lower part of this range.",
+  "In general, the greenhouse effect is a good thing. Without greenhouse gases, the temperature on Earth would be too cold to support life. With too many, it would heat up beyond survivable levels. Earth is sometimes called the Goldilocks Planet because it is \"just right.\"",
+  "Mexican children do not receive presents on Christmas Day. They receive gifts on January 6, the day on which Mexicans celebrate the arrival of the Three Wise Men.",
+  "Mexico City is built over the ruins of a great Aztec city, Tenochtitlán. Because it is built on a lake, Mexico is sinking at a rate of 6 to 8 inches a year as pumps draw water out for the city’s growing population.",
+  "Mexico’s flag is made up of three vertical stripes. The left green stripe stands for hope, the middle white stripe represents purity, and the right red stripe represents the blood of the Mexican people. The picture of an eagle eating a snake is based on an Aztec legend."
 ];
 
 /*
